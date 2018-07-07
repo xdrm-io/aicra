f3b442e810290f12a218f3841adbd9ba26f203f8
xdrm-brackets <xdrm.brackets.dev@gmail.com>
2018-07-07 12:56:03 +0200

    add format title+alignment

--- clifmt/symbols.go
+++ clifmt/symbols.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+var title_index = 0
+var align_offset = 30
+
 func Warn(s ...string) string {
 	if len(s) == 0 {
 		return Color(31, "/!\\")
@@ -17,4 +20,17 @@ func Info(s ...string) string {
 	}
 
 	return fmt.Sprintf("%s  %s", Info(), s[0])
+}
+
+func Title(s string) {
+	title_index++
+	fmt.Printf("\n%s (%d) %s %s\n", Color(33, ">>", false), title_index, s, Color(33, "<<", false))
+
+}
+
+func Align(s string) {
+	fmt.Printf("%s", s)
+	for i := len(s); i < align_offset; i++ {
+		fmt.Printf(" ")
+	}
 }
