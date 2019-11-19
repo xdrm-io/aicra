44d19e8b65c7058f8d74802e86934ee6cb4c431c
xdrm-brackets <xdrm.brackets.dev@gmail.com>
2019-11-19 16:15:44 +0100

    Test store with uri arguments

--- internal/reqdata/store_test.go
+++ internal/reqdata/store_test.go
@@ -32,3 +32,37 @@ func TestEmptyStore(t *testing.T) {
 		t.Fail()
 	}
 }
+
+func TestStoreWithUri(t *testing.T) {
+	urilist := []string{"abc", "def"}
+	store := New(urilist, nil)
+
+	if len(store.URI) != len(urilist) {
+		t.Errorf("store 'Set' should contain %d elements (got %d)", len(urilist), len(store.URI))
+		t.Fail()
+	}
+	if len(store.Set) != len(urilist) {
+		t.Errorf("store 'Set' should contain %d elements (got %d)", len(urilist), len(store.Set))
+		t.Fail()
+	}
+
+	for i, value := range urilist {
+
+		t.Run(fmt.Sprintf("URL#%d='%s'", i, value), func(t *testing.T) {
+			key := fmt.Sprintf("URL#%d", i)
+			element, isset := store.Set[key]
+
+			if !isset {
+				t.Errorf("store should contain element with key '%s'", key)
+				t.Failed()
+			}
+
+			if element.Value != value {
+				t.Errorf("store[%s] should return '%s' (got '%s')", key, value, element.Value)
+				t.Failed()
+			}
+		})
+
+	}
+
+}
