5dad0ecc3909a678ccf9277fca99abab2c71e20a
xdrm-brackets <xdrm.brackets.dev@gmail.com>
2020-03-02 22:42:45 +0100

    test string slice vs. json string slice

--- internal/reqdata/parameter_test.go
+++ internal/reqdata/parameter_test.go
@@ -32,8 +32,52 @@ func TestSimpleString(t *testing.T) {
 	}
 }
 
-func TestStringSlice(t *testing.T) {
+func TestJsonStringSlice(t *testing.T) {
 	p := Parameter{Parsed: false, File: false, Value: `["str1", "str2"]`}
+
+	err := p.Parse()
+
+	if err != nil {
+		t.Errorf("unexpected error: <%s>", err)
+		t.FailNow()
+	}
+
+	if !p.Parsed {
+		t.Errorf("expected parameter to be parsed")
+		t.FailNow()
+	}
+
+	slice, canCast := p.Value.([]interface{})
+	if !canCast {
+		t.Errorf("expected parameter to be a []interface{}")
+		t.FailNow()
+	}
+
+	if len(slice) != 2 {
+		t.Errorf("expected 2 values, got %d", len(slice))
+		t.FailNow()
+	}
+
+	results := []string{"str1", "str2"}
+
+	for i, res := range results {
+
+		cast, canCast := slice[i].(string)
+		if !canCast {
+			t.Errorf("expected parameter %d to be a []string", i)
+			continue
+		}
+		if cast != res {
+			t.Errorf("expected first value to be '%s', got '%s'", res, cast)
+			continue
+		}
+
+	}
+
+}
+
+func TestStringSlice(t *testing.T) {
+	p := Parameter{Parsed: false, File: false, Value: []string{"str1", "str2"}}
 
 	err := p.Parse()
 
