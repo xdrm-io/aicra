6a144a9a93ac0573a0a1cbe29d118fd3bb409b79
xdrm-brackets <xdrm.brackets.dev@gmail.com>
2020-03-14 16:16:30 +0100

    rename Builder into semantic DataType

--- config/datatype/types.go
+++ config/datatype/types.go
@@ -4,9 +4,9 @@ package datatype
 // and casts the value into a compatible type
 type Validator func(value interface{}) (cast interface{}, valid bool)
 
-// Builder builds a DataType from the type definition (from the
+// DataType builds a DataType from the type definition (from the
 // configuration field "type") and returns NIL if the type
 // definition does not match this DataType
-type Builder interface {
+type DataType interface {
 	Build(typeDefinition string) Validator
 }
