e0105440f8ce5d4acf9a6d7fe6f7949116bc2afc
xdrm <xdrm.dev@gmail.com>
2023-06-25 20:39:27 +0200

    feat: create runtime parameter extraction

---
+++ runtime/errors.go
@@ -0,0 +1,32 @@
+package runtime
+
+// Err defines const-enabled errors with type boxing
+type Err string
+
+// Error implements error
+func (err Err) Error() string {
+	return string(err)
+}
+
+const (
+	// ErrInvalidMultipart is returned when multipart parse failed
+	ErrInvalidMultipart = Err("invalid multipart")
+
+	// ErrParseParameter is returned when a parameter fails when parsing
+	ErrParseParameter = Err("cannot parse parameter")
+
+	// ErrInvalidJSON is returned when json parse failed
+	ErrInvalidJSON = Err("invalid json")
+
+	// ErrMissingRequiredParam - required param is missing
+	ErrMissingRequiredParam = Err("missing required param")
+
+	// ErrInvalidType - parameter value does not satisfy its type
+	ErrInvalidType = Err("invalid type")
+
+	// ErrMissingURIParameter - missing an URI parameter
+	ErrMissingURIParameter = Err("missing URI parameter")
+
+	// ErrUnhandledContentType - when an unknown Content-Type is encountered
+	ErrUnhandledContentType = Err("unhandled Content-Type header")
+)
---
+++ runtime/extract.go
@@ -0,0 +1,116 @@
+package runtime
+
+import (
+	"net/http"
+	"net/url"
+	"reflect"
+
+	"github.com/xdrm-io/aicra/internal/config"
+	"github.com/xdrm-io/aicra/validator"
+)
+
+// ExtractURI extracts an URI parameter from an http request
+func ExtractURI[T any](r *http.Request, i int, extractor validator.ExtractFunc[T]) (T, error) {
+	zero := *new(T)
+
+	fragments := config.URIFragments(r.RequestURI)
+	if i >= len(fragments) {
+		return zero, ErrMissingURIParameter
+	}
+
+	v, ok := extractor(fragments[i])
+	if !ok {
+		return zero, ErrInvalidType
+	}
+	return v, nil
+}
+
+// ExtractQuery extracts an Query parameter from an http request
+func ExtractQuery[T any](r *http.Request, name string, optional bool, extractor validator.ExtractFunc[T]) (T, error) {
+	zero := *new(T)
+
+	query, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		return zero, err
+	}
+
+	values, ok := query[name]
+	if !ok && !optional {
+		return zero, ErrMissingRequiredParam
+	}
+
+	value, err := extractFromStringList[T](values)
+	if err != nil {
+		return zero, err
+	}
+	v, ok := extractor(value)
+	if !ok {
+		return zero, ErrInvalidType
+	}
+	return v, nil
+}
+
+// ExtractForm extracts a Form parameter from an http request
+func ExtractForm[T any](form Form, name string, optional bool, extractor validator.ExtractFunc[T]) (T, error) {
+	zero := *new(T)
+
+	switch form.typ {
+	case JSON:
+		value, ok := form.values[name]
+		if !ok && !optional {
+			return zero, ErrMissingRequiredParam
+		}
+		v, ok := extractor(value)
+		if !ok {
+			return zero, ErrInvalidType
+		}
+		return v, nil
+
+	case URLEncoded:
+		raw, ok := form.values[name]
+		if !ok && !optional {
+			return zero, ErrMissingRequiredParam
+		}
+		values, ok := raw.([]string)
+		if !ok {
+			return zero, ErrParseParameter
+		}
+		value, err := extractFromStringList[T](values)
+		if err != nil {
+			return zero, err
+		}
+		v, ok := extractor(value)
+		if !ok {
+			return zero, ErrInvalidType
+		}
+		return v, nil
+
+	case Multipart:
+		value, ok := form.values[name]
+		if !ok && !optional {
+			return zero, ErrMissingRequiredParam
+		}
+		v, ok := extractor(value)
+		if !ok {
+			return zero, ErrInvalidType
+		}
+		return v, nil
+
+	}
+	return zero, ErrUnhandledContentType
+}
+
+// extractFromStringList extracts values from a string list. If the expected
+// type is a slice it reads all values ; otherwise it only expects a single value.
+func extractFromStringList[T any](values []string) (any, error) {
+	zero := *new(T)
+
+	// expect slice
+	if reflect.TypeOf(zero).Kind() == reflect.Slice {
+		return values, nil
+	}
+	if len(values) != 1 {
+		return zero, ErrParseParameter
+	}
+	return values[0], nil
+}
