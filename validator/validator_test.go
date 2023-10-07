5b1ebc2eaefc671afea0534e96d8033756b59a07
xdrm <xdrm.dev@gmail.com>
2023-10-07 09:58:29 +0200

    test: validator to 100% coverage

--- validator/validator_test.go
+++ validator/validator_test.go
@@ -35,3 +35,70 @@ func testValidator[T any](t *testing.T, validator validator.Validator[T], tt []t
 		})
 	}
 }
+
+func TestValidator_Wrap(t *testing.T) {
+	tt := []struct {
+		name        string
+		val         validator.Validator[any]
+		params      []string
+		validParams bool
+
+		value     any
+		extracted any
+		valid     bool
+	}{
+		{
+			name:  "string no params valid",
+			val:   validator.Wrap[string](new(validator.String)),
+			value: "13char string",
+
+			validParams: true,
+			extracted:   "13char string",
+			valid:       true,
+		},
+		{
+			name:        "string 2 params invalid",
+			val:         validator.Wrap[string](new(validator.String)),
+			params:      []string{"1", "5"},
+			validParams: true,
+			value:       "13char string",
+			valid:       false,
+		},
+		{
+			name:        "string 1 param valid",
+			val:         validator.Wrap[string](new(validator.String)),
+			params:      []string{"13"},
+			validParams: true,
+			value:       "13char string",
+			valid:       true,
+			extracted:   "13char string",
+		},
+		{
+			name:        "invalid uint unexpected params",
+			val:         validator.Wrap[uint](new(validator.Uint)),
+			params:      []string{"unexpected"},
+			validParams: false,
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			tc := tc
+			t.Parallel()
+
+			extractor := tc.val.Validate(tc.params)
+			if !tc.validParams {
+				require.Nil(t, extractor, "extractor")
+				return
+			}
+			require.NotNil(t, extractor, "extractor")
+
+			extracted, valid := extractor(tc.value)
+			require.Equal(t, tc.valid, valid, "valid")
+			if !tc.valid {
+				return
+			}
+			require.Equal(t, tc.extracted, extracted, "extracted")
+		})
+	}
+}
